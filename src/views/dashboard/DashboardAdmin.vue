007eaebc711f6142be476440dc3eb0f805ecd36f
andraswebcode <andras.tovishati@gmail.com>
2024-10-23 18:42:12 +0200

    Made some changes

--- src/views/dashboard/DashboardAdmin.vue
+++ src/views/dashboard/DashboardAdmin.vue
@@ -1,8 +1,54 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+import useRequest from './../../hooks/request';
+import { useNotice } from './../../store';
+
+const { save } = useRequest();
+const notice = useNotice();
+const assets = ref('');
+const loading = ref(false);
+
+const saveAssets = () => {
+	let data = null;
+
+	try {
+		data = JSON.parse(assets.value);
+	} catch (e) {
+		//
+	}
+
+	if (!data) {
+		notice.send('JSON is not valid.', 'error');
+		return;
+	}
+
+	loading.value = true;
+
+	save(
+		'',
+		'assets',
+		data,
+		(response) => {
+			console.log(response);
+			loading.value = false;
+			notice.send(`${response.count} assets added successfully.`, 'success');
+		},
+		(error) => {
+			console.warn(error);
+			loading.value = false;
+			notice.send(error.response?.data?.message || error.message, 'error');
+		}
+	);
+};
+</script>
 
 <template>
-	<p>Admin settings/tools</p>
-	<p>upload assets, etc.</p>
+	<VCard>
+		<VCardItem>
+			<VTextarea label="Asset JSON" v-model="assets" />
+			<VBtn @click="saveAssets" :loading="loading">Save</VBtn>
+		</VCardItem>
+	</VCard>
 </template>
 
 <style scoped lang="scss"></style>
