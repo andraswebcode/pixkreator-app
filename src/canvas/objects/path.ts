54b7eb87d38d6eead64f6f5ba4d6ccd4388939d1
andraswebcode <andras.tovishati@gmail.com>
2025-01-13 16:35:42 +0100

    simplified path data

--- src/canvas/objects/path.ts
+++ src/canvas/objects/path.ts
@@ -1,9 +1,18 @@
-import { controlsUtils, Path } from 'fabric';
+import { controlsUtils, Path, util } from 'fabric';
 import { Defaults } from '../mixins/defaults';
+import { toFixed } from '../../utils/functions';
 
 class PROPath extends Defaults(Path) {
 	_getModifyControls() {
 		return controlsUtils.createPathControls(this);
+	}
+	_setPath(path: util.TComplexPathData | string, adjustPosition?: boolean) {
+		const _path = util.makePathSimpler(Array.isArray(path) ? path : util.parsePath(path));
+
+		// @ts-ignore
+		this.path = _path.map((c) => c.map((n) => (typeof n === 'number' ? toFixed(n) : n)));
+
+		this.setBoundingBox(adjustPosition);
 	}
 }
 
