9ebf8f84cd045607c6ff7529dc601c6dbc1b30ce
andraswebcode <andras.tovishati@gmail.com>
2025-01-14 13:59:20 +0100

    Set nonScaling to true

--- src/canvas/shadow.ts
+++ src/canvas/shadow.ts
@@ -2,13 +2,15 @@ import { Shadow, TClassProperties } from 'fabric';
 
 const defaults: Partial<TClassProperties<Shadow>> = {
 	...Shadow.ownDefaults,
-	affectStroke: true
+	affectStroke: true,
+	nonScaling: true
 };
 
 class PROShadow extends Shadow {
 	static ownDefaults = defaults;
 
 	affectStroke = true;
+	nonScaling = true;
 }
 
 export { PROShadow };
